49f1d095ece258f68aa82b1ef1c60d7bc1dc5f3c
„Enes <enes.gnay@yahoo.de>
2025-07-03 12:45:22 +0200

    sound added

--- js/main.js
+++ js/main.js
@@ -6,12 +6,13 @@ const startBtn = document.getElementById('startBtn');
 const restartBtn = document.getElementById('restartBtn');
 const player = document.getElementById('player');
 const obstacleContainer = document.getElementById('obstacleContainer');
+const sound = document.getElementById('sound');
+const jumpSound = document.getElementById('jumpSound')
 
 let gameRunning = false;
 let jumpTimeout;
 let obstacleInterval;
 let gameLoopId;
-
 
 startBtn.addEventListener('click', startGame);
 restartBtn.addEventListener('click', startGame);
@@ -21,13 +22,15 @@ function startGame() {
   startScreen.classList.add('hidden');
   gameOverScreen.classList.add('hidden');
   gameWrapper.classList.remove('hidden');
-
   obstacleContainer.innerHTML = '';
   player.style.bottom = '20px';
 
-  gameRunning = true;
+  gameRunning = true
+// musik wird direkt anch start klick gespielt
+  sound.currentTime=0;    // falls neu gestartet wird
+  sound.play()
 
-  document.addEventListener('keydown', handleJump); 
+  document.addEventListener('keydown', handleJump);
 
   obstacleInterval = setInterval(spawnObstacle, 1500);
   gameLoopId = requestAnimationFrame(gameLoop);
@@ -45,6 +48,8 @@ function handleJump(e) {
       player.style.bottom = '20px';
       jumpTimeout = null;
     }, 420);
+     jumpSound.currentTime=0;    // falls neu gestartet wird
+     jumpSound.play()
   }
 }
 
@@ -67,6 +72,7 @@ function gameLoop() {
     right += 5;
     obs.style.right = right + 'px';
 
+
     if (right > window.innerWidth + 50) {
       obs.remove();
     }
@@ -84,9 +90,9 @@ function checkCollision(a, b) {
   const r1 = a.getBoundingClientRect();
   const r2 = b.getBoundingClientRect();
   return !(r1.right < r2.left ||
-           r1.left > r2.right ||
-           r1.bottom < r2.top ||
-           r1.top > r2.bottom);
+    r1.left > r2.right ||
+    r1.bottom < r2.top ||
+    r1.top > r2.bottom);
 }
 
 
@@ -95,7 +101,12 @@ function endGame() {
   cancelAnimationFrame(gameLoopId);
   clearInterval(obstacleInterval);
 
-  document.removeEventListener('keydown', handleJump); 
+  document.removeEventListener('keydown', handleJump);
+
+  //musik stoppen
+  sound.pause()
+  sound.currentTime=0;     // zurückspulen zum anfang /neustart
+
 
   gameWrapper.classList.add('hidden');
   gameOverScreen.classList.remove('hidden');
